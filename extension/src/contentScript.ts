6dbd5a599598a1b18396dc6dcdd4749136f52229
evelyn masso <coding@outofambit.com>
2020-08-25 15:08:11 -0700

    add alt property

--- extension/src/contentScript.ts
+++ extension/src/contentScript.ts
@@ -141,7 +141,8 @@ function listenToInput(
           knownToken.token.value
         )}"><img src="${
           knownToken.imageUrl
-        }" title="Created by gitme.me with /${knownToken.token.value}"/></a>`;
+        }" title="Created by gitme.me with /${knownToken.token.value}"
+        alt="${knownToken.token.value}"/></a>`;
         value =
           value.substring(0, knownToken.token.index) +
           tagInsert +
