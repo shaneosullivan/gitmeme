359b9e5150e208d72224d3e9f74b6f41593502fd
Shane O'Sullivan <shaneosullivan1@gmail.com>
2023-11-15 13:19:39 +0000

    Add note about the Github app

--- shared/auth/getToken.ts
+++ shared/auth/getToken.ts
@@ -10,6 +10,8 @@ declare const chrome: any;
 
 console.log("getToken file loaded");
 
+// The Gitmeme Github app for auth is at https://github.com/settings/applications/1090737
+
 export function getTokenHeadless(
   interactive: boolean,
   callback: Function,
